55727ec8eb3dd25c44e98ce397796fb5cabd54a5
Gael Rivera <gri@capraconsulting.no>
2023-04-03 22:01:21 +0200

    favicon and comments

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
+// Wait for the DOM to load
 document.addEventListener('DOMContentLoaded', function () {
-    const hamburgerMenu = document.querySelector('.hamburger-menu');
-    const mobileMenu = document.createElement('ul');
-    mobileMenu.classList.add('mobile-menu');
-  
-    const menuItems = document.querySelectorAll('.menu li');
-    menuItems.forEach((item) => {
+  // Select the hamburger menu element
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+  // Create the mobile menu element
+  const mobileMenu = document.createElement('ul');
+  mobileMenu.classList.add('mobile-menu');
+
+  // Clone desktop menu items to the mobile menu
+  const menuItems = document.querySelectorAll('.menu li');
+  menuItems.forEach((item) => {
       const mobileItem = document.createElement('li');
       mobileItem.appendChild(item.querySelector('a').cloneNode(true));
       mobileMenu.appendChild(mobileItem);
-    });
-  
-    document.querySelector('nav').appendChild(mobileMenu);
-  
-    hamburgerMenu.addEventListener('click', function () {
-      mobileMenu.classList.toggle('show');
-    });
   });
-  
+
+  // Add the mobile menu to the navigation
+  document.querySelector('nav').appendChild(mobileMenu);
+
+  // Toggle the mobile menu visibility when the hamburger menu is clicked
+  hamburgerMenu.addEventListener('click', function () {
+      mobileMenu.classList.toggle('show');
+  });
+});
