2478da29216e5e834c9bffc4928b118beea53b84
Gael Rivera <gael.rivera@hotmail.com>
2026-07-09 21:11:40 +0200

    enhanced post12 with additional details and formatting improvements

---
+++ src/config/site.ts
@@ -0,0 +1,26 @@
+export const site = {
+  name: 'Gael Rivera',
+  description:
+    'Join me as I sharpen my writing skills, embrace humor, and share my journey of learning, growth, and entrepreneurship through projects, ideas, and a healthy dose of trial and error. Welcome to my unfiltered adventure!',
+  blogName: 'The good enough blog',
+  url: 'https://gaelrivera.com',
+  nav: [
+    { label: 'About', href: '/about' },
+    { label: 'Blog', href: '/blog' },
+    { label: 'Projects', href: '/projects' },
+  ],
+  social: [
+    {
+      label: '@Gael_Rivera',
+      href: 'https://twitter.com/Gael_Rivera',
+    },
+    {
+      label: 'linkedin.com/in/gaelrivera',
+      href: 'https://linkedin.com/in/gaelrivera',
+    },
+    {
+      label: 'github.com/GaelRR',
+      href: 'https://github.com/GaelRR',
+    },
+  ],
+} as const;
